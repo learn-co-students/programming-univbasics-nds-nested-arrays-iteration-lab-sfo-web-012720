3ed566e20e6058be2466dfa882b6ccd67b1b7b90
Raymond Michael Morales <michaelmorales53@gmail.com>
2020-01-19 04:40:23 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,45 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  
+  stringArray = []
+  
+  for element in src
+  
+    sentence = "I love #{element[0]} and #{element[1]} on my pizza"
+    stringArray.push(sentence)
+  
+  end
+  
+  stringArray
+  
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  
+  numberArray = []
+  
+  for element in src
+  
+    numberArray.push(element.max)
+  
+  end
+  
+  numberArray
+  
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  
+  arraySum = 0
+  
+  for element in src
+    
+    if element[0].even? && element[1].even?
+      
+      arraySum += element[0] + element[1]
+      
+    end
+  end    
+  
+  arraySum
+  
 end
